d6fe2e5849647ef3bc12c07485a3bb72702ddb05
Viswanath K <sunonebytes@gmail.com>
2023-07-23 21:56:27 +0530

    iniital commit

---
+++ src/main/java/com/consumer/auto/star/model/Star5XsdJavaPojoApplication.java
@@ -0,0 +1,13 @@
+package com.consumer.auto.star.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Star5XsdJavaPojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Star5XsdJavaPojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumer/auto/star/model/Star5XsdJavaPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer.auto.star.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Star5XsdJavaPojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
